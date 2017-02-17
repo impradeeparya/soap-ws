56a0cb27aa10cff0d3303b9bf9bddb5a791c9af6
Pradeep Arya <impradeeparya@gmail.com>
2017-02-18 00:22:19 +0530

    wsdl exposed and end point defined to process SOAP request

--- src/main/java/com/soap/SoapWsApplication.java
+++ src/main/java/com/soap/SoapWsApplication.java
@@ -1,12 +1,32 @@
 package com.soap;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SoapWsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoapWsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoapWsApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Beans created by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
 }
---
+++ src/main/java/com/soap/configuration/WebServiceConfig.java
@@ -0,0 +1,57 @@
+package com.soap.configuration;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+/**
+ * Created by with IntelliJ IDEA.
+ * User: pradeep
+ * Date: 17/2/17
+ * Time: 5:13 PM
+ */
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    /*
+    * For handling SOAP messages Spring WS uses MessageDispatcherServlet
+    * Inject applicationContext into MessageDispatcherServlet to detect spring beans automatically.
+    * will not replace default dispatcher servlet
+    * */
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+
+    /*
+    * Expose standard ws to client
+    * */
+    @Bean(name = "countries")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CountriesPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://spring.io/soap/ws");
+        wsdl11Definition.setSchema(countriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/countries.xsd"));
+    }
+}
