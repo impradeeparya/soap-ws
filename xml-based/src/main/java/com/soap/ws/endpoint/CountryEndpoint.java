32d066ba282c8da74ad3707e34b960e1fa3b0831
Pradeep Arya <impradeeparya@gmail.com>
2017-04-08 17:31:52 +0530

    xml based spring ws changes

--- xml-based/src/main/java/com/soap/ws/endpoint/CountryEndpoint.java
+++ xml-based/src/main/java/com/soap/ws/endpoint/CountryEndpoint.java
@@ -3,29 +3,37 @@ package com.soap.ws.endpoint;
 import com.soap.ws.repository.CountryRepository;
 import com.soap.ws.xjc.GetCountryRequest;
 import com.soap.ws.xjc.GetCountryResponse;
+import org.springframework.ws.server.endpoint.AbstractMarshallingPayloadEndpoint;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 
 /**
  * Created by pradeep on 7/4/17.
  */
 
-@Endpoint
-public class CountryEndpoint {
+//@Endpoint
+public class CountryEndpoint extends AbstractMarshallingPayloadEndpoint {
 
-    private static final String NAMESPACE_URI = "http://soap.com/ws/xjc";
-    private static final String GET_COUNTRY_REQUEST = "getCountryRequest";
+   /* private static final String NAMESPACE_URI = "http://soap.com/ws/xjc";
+    private static final String GET_COUNTRY_REQUEST = "getCountryRequest";*/
 
     private CountryRepository countryRepository;
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = GET_COUNTRY_REQUEST)
+    /*@PayloadRoot(namespace = NAMESPACE_URI, localPart = GET_COUNTRY_REQUEST)
     public GetCountryResponse getCountry(GetCountryRequest request) {
         GetCountryResponse response = new GetCountryResponse();
         response.setCountry(countryRepository.findCountry(request.getName()));
         return response;
-    }
+    }*/
 
     public void setCountryRepository(CountryRepository countryRepository) {
         this.countryRepository = countryRepository;
+    }
+
+    @Override
+    protected Object invokeInternal(Object o) throws Exception {
+        GetCountryRequest request = (GetCountryRequest) o;
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+        return response;
     }
 }
